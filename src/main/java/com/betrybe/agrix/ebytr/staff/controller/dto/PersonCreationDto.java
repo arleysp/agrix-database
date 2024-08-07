81ed2c0d9ba9b8459083553a8b8b3b449acc4371
Arley Pereira <arleyspereira@hotmail.com>
2024-08-07 18:55:11 -0300

    Feat: Requisito 2 - Rota Post Person

---
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controller/dto/PersonCreationDto.java
@@ -0,0 +1,29 @@
+package com.betrybe.agrix.ebytr.staff.controller.dto;
+
+import com.betrybe.agrix.ebytr.staff.entity.Person;
+import com.betrybe.agrix.ebytr.staff.security.Role;
+import org.springframework.security.core.parameters.P;
+
+/**
+ * The type Person creation dto.
+ */
+public record PersonCreationDto(
+    String username,
+    String password,
+    Role role
+) {
+
+  /**
+   * To entity person.
+   *
+   * @return the person
+   */
+  public Person toEntity() {
+    Person person = new Person();
+    person.setUsername(username);
+    person.setPassword(password);
+    person.setRole(role);
+
+    return person;
+  }
+}
